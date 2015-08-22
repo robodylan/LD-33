48dcecf191f852b325b6fd362b885c4039e913ec
Dylan Dunn <robodylan123@gmail.com>
2015-08-22 10:26:33 -0400

    Added entities and movement of entitites

---
+++ LD 33/Entity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LD_33
+{
+    public class Entity
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+        public int ID { get; set; }
+        public enum Movement { Back, Forward, Right, Left}
+        public Entity(int x, int y, int width, int height,int ID)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.ID = ID;
+        }
+    }
+}
--- LD 33/Game1.cs
+++ LD 33/Game1.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -10,17 +12,23 @@ namespace LD_33
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        List<Tile> tiles = new List<Tile>();
+        List<Entity> entities;
+        List<Tile> tiles;
         Texture2D tileTexture;
+        Vector2 offset;
+        Entity player;
 
         public Game1()
         {
+            player = new Entity(0, 0, 16, 16, 1);
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
 
         protected override void Initialize()
         {
+            tiles = new List<Tile>();
+            entities = new List<Entity>();
             base.Initialize();
         }
 
@@ -28,21 +36,22 @@ namespace LD_33
         {
             tileTexture = Content.Load<Texture2D>("tiles");
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            tiles.Add(new Tile(0, 0, 0, true));
-            tiles.Add(new Tile(1, 0, 1, true));
-            tiles.Add(new Tile(2, 0, 2, true));
+            LoadMap(1);
         }
 
         protected override void UnloadContent()
         {
 
-        };
+        }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
+            if (Keyboard.GetState().IsKeyDown(Keys.W)) Move(player, Entity.Movement.Forward);
+            if (Keyboard.GetState().IsKeyDown(Keys.A)) Move(player, Entity.Movement.Back);
+            if (Keyboard.GetState().IsKeyDown(Keys.S)) Move(player, Entity.Movement.Left);
+            if (Keyboard.GetState().IsKeyDown(Keys.D)) Move(player, Entity.Movement.Right);
             base.Update(gameTime);
         }
 
@@ -54,8 +63,49 @@ namespace LD_33
             {
                 spriteBatch.Draw(tileTexture, new Vector2(tile.x * 64, tile.y * 64), new Rectangle((tile.ID % tileTexture.Width) * 16,(tile.ID / tileTexture.Width) * 16,16,16),new Color(255,255,255,128), 0, new Vector2(0,0), 4, SpriteEffects.None, 0);
             }
+            foreach (Entity entity in entities)
+            {
+                spriteBatch.Draw(tileTexture, new Vector2(entity.x * 64, entity.y * 64), new Rectangle((entity.ID % tileTexture.Width) * 16, (entity.ID / tileTexture.Width) * 16, 16, 16), new Color(255, 255, 255, 128), 0, new Vector2(0, 0), 4, SpriteEffects.None, 0);
+            }
             spriteBatch.End();
             base.Draw(gameTime);
+        }
+
+        public void Move(Entity entity, Entity.Movement movement)
+        {
+            switch(movement)
+            {
+                case Entity.Movement.Forward:
+                    break;
+                case Entity.Movement.Left:
+                    break;
+                case Entity.Movement.Back:
+                    break;
+                case Entity.Movement.Right:
+                    break;
+            }
+        }
+
+        public void LoadMap(int i)
+        {
+            string[] mapData = File.ReadAllLines("Content/Maps/Map" + i + ".map");
+
+            foreach (string blockData in mapData)
+            {
+                try
+                {
+                    string[] properties = blockData.Split(',');
+                    int x = Convert.ToInt32(properties[0]);
+                    int y = Convert.ToInt32(properties[1]);
+                    int ID = Convert.ToInt32(properties[2]);
+                    bool hasPhysics = true;
+                    tiles.Add(new Tile(x, y, ID, hasPhysics));
+                }
+                catch
+                {
+
+                }
+            }
         }
     }
 }
