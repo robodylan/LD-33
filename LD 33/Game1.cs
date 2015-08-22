d2cc5509245ed99ea39dc8d0c651147973abaff6
Dylan Dunn <robodylan123@gmail.com>
2015-08-21 23:00:26 -0400

    Added tile class and drawing of tiles

---
+++ LD 33/Game1.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace LD_33
+{
+
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        List<Tile> tiles = new List<Tile>();
+        Texture2D tileTexture;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            tileTexture = Content.Load<Texture2D>("tiles");
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            tiles.Add(new Tile(0, 0, 0, true));
+            tiles.Add(new Tile(1, 0, 1, true));
+            tiles.Add(new Tile(2, 0, 2, true));
+        }
+
+        protected override void UnloadContent()
+        {
+
+        };
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin(SpriteSortMode.Deferred,null,SamplerState.PointClamp,null,null,null,null);
+            foreach (Tile tile in tiles)
+            {
+                spriteBatch.Draw(tileTexture, new Vector2(tile.x * 64, tile.y * 64), new Rectangle((tile.ID % tileTexture.Width) * 16,(tile.ID / tileTexture.Width) * 16,16,16),new Color(255,255,255,128), 0, new Vector2(0,0), 4, SpriteEffects.None, 0);
+            }
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ LD 33/Tile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LD_33
+{
+    class Tile
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int ID { get; set; }
+        public bool hasPhysics { get; set; }
+        public Tile(int x, int y, int ID, bool hasPhysics)
+        {
+            this.x = x;
+            this.y = y;
+            this.ID = ID;
+            this.hasPhysics = hasPhysics;
+        }
+    }
+}
